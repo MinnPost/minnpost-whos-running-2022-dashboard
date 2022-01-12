8d6e666cc4b4790b6746799289288152ffb416ef
Tom Nehil <tnehil@minnpost.com>
2022-01-12 08:16:18 -0600

    Adds basics of svelte app

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+	import { onMount } from "svelte";
+
+	let allRaces =[];
+	let races = [];
+
+	onMount(async function() {
+		const response = await fetch(` https://s3.amazonaws.com/data.minnpost/projects/minnpost-whos-running-2022/candidate-tracker-sample-data.json`);
+		const json = await response.json();
+		races = json;
+	});
+</script>
+
+{#if races.length === 0}
+	<div class="loading-container">
+		Races loading...
+	</div> 
+{:else}
+	{#each races as race}
+		<h2>{race.office}</h2>
+	{/each}
+{/if}
