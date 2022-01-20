0e72c82eeb4caf7c0adbf65501a0e68ad1947572
Jonathan Stegall <jstegall@minnpost.com>
2022-01-20 16:09:14 -0600

    clean up the party candidate list

--- src/CandidateList.svelte
+++ src/CandidateList.svelte
@@ -1,8 +1,16 @@
 <script>
-	import PartyCandidates from "./PartyCandidates.svelte";
 
-	// candidates from App.svelte
+	import Candidate from "./Candidate.svelte";
+
+	// load the candidates from App.svelte
 	export let candidates;
+
+	// create a list of candidates for a party
+	let party_candidates = function(party) {
+		return candidates.filter(
+			(item) => item["party"].toUpperCase().indexOf(party.toUpperCase()) !== -1
+		);
+	}
 
 	// the distinct party names from the candidates
 	let parties = [...new Set(candidates.map(item => item.party))];
@@ -12,10 +20,8 @@
 {#each parties as party}
 	<section class="candidates-list">
 		<h3>{party}</h3>
-		<PartyCandidates
-			party_candidates = {candidates.filter(
-				(item) => item["party"].toUpperCase().indexOf(party.toUpperCase()) !== -1
-			)}
-		/>
+		{#each party_candidates(party) as candidate}
+			<Candidate candidate = {candidate} />
+		{/each}
 	</section>
 {/each}
